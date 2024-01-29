940effa4beec36aafa638f1151a2ea45e1780b16
Kevin <junho2413@gmail.com>
2024-01-29 16:41:27 -0500

    # test

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { Theme } from '@radix-ui/themes'
 import 'tailwindcss/tailwind.css'
 import '@radix-ui/themes/styles.css'
 import App from 'components/App'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
@@ -13,5 +12,3 @@ root.render(
     <App />
   </Theme>
 )
-
-serviceWorkerRegistration.register()
---
+++ sw.js
@@ -0,0 +1,17 @@
+// install event
+self.addEventListener('install', (e) => {
+  import.meta.env.VITE_REACT_APP_ENV !== 'production' &&
+    console.log('[Service Worker] installed')
+})
+
+// activate event
+self.addEventListener('activate', (e) => {
+  import.meta.env.VITE_REACT_APP_ENV !== 'production' &&
+    console.log('[Service Worker] actived', e)
+})
+
+// fetch event
+self.addEventListener('fetch', (e) => {
+  import.meta.env.VITE_REACT_APP_ENV !== 'production' &&
+    console.log('[Service Worker] fetched resource ' + e.request.url)
+})
