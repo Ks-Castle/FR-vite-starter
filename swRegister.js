060ff221ab0aea73426819cafedd897c2544dbd2
Kevin <junho2413@gmail.com>
2024-01-29 16:46:28 -0500

    # test

--- swRegister.js
+++
@@ -1,6 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    import.meta.env.VITE_REACT_APP_ENV !== 'production' &&
-      navigator.serviceWorker.register('/sw.js', { scope: '/' })
-  })
-}
