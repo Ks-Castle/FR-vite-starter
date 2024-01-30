830350dbe837e2a3a20ad4d804ed3a565c85b89a
Kevin <junho2413@gmail.com>
2024-01-30 12:13:12 -0500

    # fix build errs

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -4,7 +4,7 @@ import App from './App'
 test('renders App component with Flex', () => {
   render(<App />)
 
-  const appElement = screen.getByText(/App/i)
+  const appElement = screen.getByText(/Caching/i)
   const app2Element = screen.getByText(/Test/i)
 
   expect(appElement).toBeInTheDocument()
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { Theme } from '@radix-ui/themes'
 import 'tailwindcss/tailwind.css'
 import '@radix-ui/themes/styles.css'
-import App from 'components/App'
+import App from '@/components/App'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
